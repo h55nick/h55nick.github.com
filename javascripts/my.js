75fa7d07c24a48913fc16f8b265ab7d0d2c8c9e7
Blanchet <h55nick@gmail.com>
2013-03-10 10:58:31 -0400

    working icon movement js

--- javascripts/my.js
+++ javascripts/my.js
@@ -36,16 +36,16 @@ scrollY: 48
 scrollX: 0
 */
 function twitscale(yPos){
-          var twitvert =  (yPos/8 +350);
+          var twitvert =  ( -yPos/3 +350);
           twithor= 120;
-          if (twitvert < 350){
+          if (twitvert > 530){
             twitvert = -(yPos);
-            twithor = yPos/16+120;
-            if(twithor <=70){
-              twithor =70;
+            twithor = yPos/8+150;
+            if(twithor <=68){
+              twithor =68;
             }
         }
-        var sizer = (350/(twitvert^0.5));
+        var sizer = (350/(twitvert));
         if(sizer>1){
           sizer = 1;
         }else if(sizer<0.5){
@@ -53,14 +53,15 @@ function twitscale(yPos){
         }else{
           sizer = sizer;
         }
+        console.log(yPos);
         $('#twithover').css('margin-top', twitvert +'px');
         $('#twithover').css('margin-left', twithor +'px');
         $('#twithover').css("-webkit-transform", 'scale(' + sizer+ ')');
 }
 function linkscale(yPos){
-          var linkvert =  (yPos/4 +250);
+          var linkvert =  (yPos/6 +250);
           linkhor= 120;
-          if (linkvert < 200){
+          if (linkvert < 220){
             linkvert = -(yPos) -5 ;
             linkhor = yPos/4+180;
             if(linkhor <=30){
@@ -86,9 +87,9 @@ function gitscale(yPos){
           var githor= 120;
           if (gitvert < 70){
             gitvert = -(yPos) -3 ;
-            githor = yPos+220;
-            if(githor <=-10){
-              githor =-10;
+            githor = yPos/2+180;
+            if(githor < -10){
+              githor = -10;
             }
         }
         var sizer = 150/gitvert;
