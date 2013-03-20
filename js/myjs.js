e79e6ae0a05ed1ffc26f24da353d46afbcabffa6
Blanchet <h55nick@gmail.com>
2013-03-20 14:53:04 -0400

    about started

--- js/myjs.js
+++ js/myjs.js
@@ -1,10 +1,12 @@
+
+
 var initheader= $('.header').offset().top;
 $(function(){
      $window = $(window);
      $(window).scroll(scrollr);
 
      if($('#barrow').offset().top < 300){
-        $('#arrowhold').remove();
+        $('#barrow').empty();
      }
 
 });
@@ -14,11 +16,15 @@ function scrollr(){
           //topnavscroll(yPos);
 
 
-        $('#arrowhold').css("opacity",100/(yPos+100));
-        if($('#arrowhold').offset().top > 860){
+        $('#arrowhold').css("opacity",100/(yPos*60+100));
+
+        if(yPos > 200){
+            chartit();
+        }
+        /*if($('#arrowhold').offset().top > 800){
             $('#arrowhold').css("opacity",0);
         }
-}
+*/}
 
 function topnavscroll(yPos){
   tD =  - $(window).scrollTop();
@@ -26,6 +32,110 @@ function topnavscroll(yPos){
   if(yPos >= initheader ){
    $('.header').css("margin-top", yPos -initheader + "px");
   }
-
-
 }
+
+
+
+
+function chartit(){
+      //Get context with jQuery - using jQuery's .get() method.
+      var ctx = $("#dochart").get(0).getContext("2d");
+      //This will get the first returned node in the jQuery collection.
+
+      var data = {
+        labels : ["Food","Reading","Dogs","Code","Design","Running"],
+        datasets : [
+          {
+            fillColor : "rgba(151,187,105,0.5)",
+            strokeColor : "rgba(151,187,105,1)",
+            pointColor : "rgba(151,187,105,1)",
+            pointStrokeColor : "#fff",
+            data : [60,30,35,46,35,20]
+          },
+          {
+            fillColor : "rgba(151,187,205,0.5)",
+            strokeColor : "rgba(151,187,205,1)",
+            pointColor : "rgba(151,187,205,1)",
+            pointStrokeColor : "#fff",
+            data : [50,40,19,86,27,80]
+          }
+        ]
+      }
+
+      var options = {
+        //Boolean - If we show the scale above the chart data
+        scaleOverlay : false,
+        //Boolean - If we want to override with a hard coded scale
+        scaleOverride : false,
+        //** Required if scaleOverride is true **
+        //Number - The number of steps in a hard coded scale
+        scaleSteps : null,
+        //Number - The value jump in the hard coded scale
+        scaleStepWidth : null,
+        //Number - The centre starting value
+        scaleStartValue : null,
+        //Boolean - Whether to show lines for each scale point
+        scaleShowLine : true,
+        //String - Colour of the scale line
+        scaleLineColor : "rgba(0,0,0,.3)",
+        //Number - Pixel width of the scale line
+        scaleLineWidth : 1,
+        //Boolean - Whether to show labels on the scale
+        scaleShowLabels : false,
+        //Interpolated JS string - can access value
+        scaleLabel : "<%=value%>",
+        //String - Scale label font declaration for the scale label
+        scaleFontFamily : "'Arial'",
+        //Number - Scale label font size in pixels
+        scaleFontSize : 12,
+        //String - Scale label font weight style
+        scaleFontStyle : "normal",
+        //String - Scale label font colour
+        scaleFontColor : "#000",
+        //Boolean - Show a backdrop to the scale label
+        scaleShowLabelBackdrop : true,
+        //String - The colour of the label backdrop
+        scaleBackdropColor : "rgba(255,255,255,0.75)",
+        //Number - The backdrop padding above & below the label in pixels
+        scaleBackdropPaddingY : 2,
+        //Number - The backdrop padding to the side of the label in pixels
+        scaleBackdropPaddingX : 2,
+        //Boolean - Whether we show the angle lines out of the radar
+        angleShowLineOut : true,
+        //String - Colour of the angle line
+        angleLineColor : "rgba(0,0,0,.8)",
+        //Number - Pixel width of the angle line
+        angleLineWidth : 1,
+        //String - Point label font declaration
+        pointLabelFontFamily : "'Crimson Text'",
+        //String - Point label font weight
+        pointLabelFontStyle : "normal",
+        //Number - Point label font size in pixels
+        pointLabelFontSize : 18,
+        //String - Point label font colour
+        pointLabelFontColor : "#333",
+        //Boolean - Whether to show a dot for each point
+        pointDot : true,
+        //Number - Radius of each point dot in pixels
+        pointDotRadius : 2,
+        //Number - Pixel width of point dot stroke
+        pointDotStrokeWidth : 1,
+        //Boolean - Whether to show a stroke for datasets
+        datasetStroke : true,
+        //Number - Pixel width of dataset stroke
+        datasetStrokeWidth : 2,
+        //Boolean - Whether to fill the dataset with a colour
+        datasetFill : true,
+        //Boolean - Whether to animate the chart
+        animation : true,
+        //Number - Number of animation steps
+        animationSteps : 600,
+        //String - Animation easing effect
+        animationEasing : "easeOutQuart",
+        //Function - Fires when the animation is complete
+        onAnimationComplete : null
+
+      }
+      var myNewChart = new Chart(ctx).Radar(data,options);
+}
+
--- old/javascripts/my.js
+++ old/javascripts/my.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
   // Cache the Window object
 
